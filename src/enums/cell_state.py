0b5ff8967636916cd596e08878a20f400de3c704
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-03-05 20:02:26 +1000

    updated docstring for cell states

--- src/enums/cell_state.py
+++ src/enums/cell_state.py
@@ -4,7 +4,8 @@ import random
 
 class CellState(Enum):
     """
-    Cell state represented by ASCII chars:
+    SUMMARY:
+        - Cell state represented by ASCII chars
         - These chars (on most OS) are proportioned 1x2
         - To represent a true square, each state is 2x a char
     """
