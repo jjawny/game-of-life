221eec9928fb9ed3edf95c2d3cef9cdc22f577cd
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-03-06 18:52:19 +1000

    cleanup main script

--- src/app.py
+++ src/app.py
@@ -37,21 +37,25 @@ def main() -> None:
     """Extracted main method to be optionally triggered by another script"""
     try:
         initial_settings = get_settings(get_cli_args())
-        menu = MenuScreen(settings=initial_settings)
-        settings = menu.show()
+        settings = MenuScreen(settings=initial_settings).show()
 
+        initial_gen = setup_matrix(settings=settings)
         is_ghost_mode = next((s.value for s in settings if s.name == "is_ghost_mode"), True)
         num_of_generations = next((s.value for s in settings if s.name == "num_of_generations"), 100)
         updates_per_s = next((s.value for s in settings if s.name == "updates_per_s"), 10)
 
-        initial_gen = setup_matrix(settings=settings)
         simulation = SimulationScreen(
             initial_gen=initial_gen,
             is_ghost_mode=is_ghost_mode,
             num_of_generations=num_of_generations,
             updates_per_s=updates_per_s,
         )
-        print("\n" + simulation.show())
+
+        all_gens = simulation.show()
+
+        if all_gens:
+            print(f"\n{all_gens[-1]}")
+            
     except KeyboardInterrupt:
         print("\nExiting...")
     except Exception as ex:
